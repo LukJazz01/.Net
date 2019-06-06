039b65324aa3a76f7a5147255287a1646e248d32
LukJazz01 <51463262+LukJazz01@users.noreply.github.com>
2019-06-06 19:37:24 -0300

    Add files via upload

---
+++ Statements/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Sentences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = null;
+
+            Console.WriteLine("Insert a number");
+            x = Console.ReadLine();
+
+            if (Char.IsLetter((char)x[0]))
+            {
+                Console.WriteLine("INSERT A NUMBER");
+            }
+
+            if (Char.IsNumber((char)x[0]))
+            {   
+
+                Console.WriteLine("Insert " +x +" numbers");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
