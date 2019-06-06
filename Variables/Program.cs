922b2ea5c833bacef10bd6569e999f7e13594639
LukJazz01 <51463262+LukJazz01@users.noreply.github.com>
2019-06-06 19:36:30 -0300

    Add files via upload

---
+++ Variables/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int INT = 1;
+            short SHORT = 2;
+            bool BOOL = false;
+            long LONG = 3;
+
+            Console.WriteLine(INT);
+            Console.WriteLine(SHORT);
+            Console.WriteLine(BOOL);
+            Console.WriteLine(LONG);
+        }
+    }
+}
