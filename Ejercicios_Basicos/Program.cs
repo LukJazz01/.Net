25944d6af46eb568a50c570a7a356c4da846fd95
LukJazz01 <51463262+LukJazz01@users.noreply.github.com>
2019-06-12 15:49:58 -0300

    Add files via upload

---
+++ Ejercicios_Basicos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ejercicios_Basicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string entry = null;
+            int i = 0;
+
+            Console.WriteLine("Enter a number");
+            entry = Console.ReadLine();
+            int number = int.Parse(entry);
+
+            if (i != number)
+            {
+                while (number > i){
+                    Console.WriteLine("Enter another number");
+                    Console.ReadLine();
+                    i++;
+                }
+            }
+        }
+    }
+}
